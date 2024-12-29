31b7d83eb40134d41496bfacbdd839ff06b531e2
Victor Kamrowski <vkamrowski50@stuy.edu>
2024-12-29 15:02:10 -0500

    cleaned up client handshake and started server handshake, first five steps done, added all necessary prints and errors

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,20 +11,20 @@
   =========================*/
 int server_setup() {
   int from_client;
-  printf("Server: Creating WKP\n");
+  printf("Server Step 1: Creating WKP\n");
   if(mkfifo(WKP,0666)==-1){
     perror("Server: Error creating WKP\n");
     exit(1);
   }
 
-  printf("Server: Opening WKP (BLOCKS)\n");
+  printf("Server Step 2: Opening WKP (BLOCKS)\n");
   from_client = open(WKP, O_RDONLY);
   if(from_client==-1){
     perror("Server: Error opening WKP\n");
     exit(1);
   }
 
-  printf("Server: Removing WKP\n");
+  printf("Server Step 4: Removing WKP\n");
   if(remove(WKP)==-1){
     perror("Server: Error removing WKP\n");
     exit(1);
@@ -42,7 +42,16 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
-  int from_client;
+  int from_client = server_setup();
+  char private_pipe[HANDSHAKE_BUFFER_SIZE];
+
+  printf("Server Step 5: Reading SYN\n");
+  if(read(from_client,private_pipe,HANDSHAKE_BUFFER_SIZE)==-1){
+    perror("Server: Error reading SYN\n");
+    exit(1);
+  }
+  printf("Server received PP: %s\n",private_pipe);
+
   return from_client;
 }
 
@@ -58,12 +67,38 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   int from_server;
-  char PP[40];
-  sprintf(PP,"%d",getpid());
-  mkfifo(PP,0666);
-  int firstopen = open(WKP,O_WRONLY);
-  write(WKP,PP,sizeof(PP));
-  int firstread = open(PP,O_RDONLY);
+  char private_pipe[HANDSHAKE_BUFFER_SIZE];
+
+  sprintf(private_pipe,"%d",getpid());
+  printf("Client Step 3: Creating PP: %s\n",private_pipe);
+  if(mkfifo(private_pipe,0666)==-1){
+    perror("Client: Error creating PP\n");
+    exit(1);
+  }
+
+  printf("Client Step 3: Opening WKP\n");
+  *to_server = open(WKP, O_WRONLY);
+  if(*to_server == -1){
+    perror("Client: Error opening WKP\n");
+    remove(private_pipe);
+    exit(1);
+  }
+
+  printf("Client Step 3: Writing PP name to WKP\n");
+  if(write(*to_server,private_pipe,sizeof(private_pipe))==-1){
+    perror("Client: Error writing PP to WKP\n");
+    remove(private_pipe);
+    exit(1);
+  }
+
+  printf("Client Step 3: Opening PP (BLOCKS)\n");
+  from_server = open(private_pipe,O_RDONLY);
+  if(from_server==-1){
+    perror("Client: Error opening PP\n");
+    remove(private_pipe);
+    exit(1);
+  }
+
   return from_server;
 }
 
