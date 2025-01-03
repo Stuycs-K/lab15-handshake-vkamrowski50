4b1a907e55d290f1f69615680eff5639fa37624d
Victor <vkamrowski50@stuy.edu>
2025-01-03 14:50:42 -0500

    not done editing makefile for testing

--- persistant_server.c
+++ persistant_server.c
@@ -1,9 +1,9 @@
 #include "pipe_networking.h"
-
+#include <signal.h>
 static void sighandler(int signo) {
     if ( signo == SIGINT ){
       printf("Removing WKP\n");
-      if(close(WKP)==-1){
+      if(remove(WKP)==-1){
         perror("Server: Error removing WKP\n");
         exit(1);
       }
@@ -14,7 +14,7 @@ static void sighandler(int signo) {
 int main() {
   int to_client;
   int from_client;
-  close(WKP);
+  remove(WKP);
   while(1){
     signal(SIGINT, sighandler);
   from_client = server_handshake( &to_client );
