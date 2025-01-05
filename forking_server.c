4e2f23f894da5b02862b45b186a5ac6be15fd247
Victor Kamrowski <vkamrowski50@stuy.edu>
2025-01-05 18:42:00 -0500

    created half handshake method, not tested

--- forking_server.c
+++ forking_server.c
@@ -12,6 +12,42 @@ static void sighandler(int signo) {
 }
 }
 
+int server_handshake_half(int *to_client,int from_client) {
+  char private_pipe[HANDSHAKE_BUFFER_SIZE];
+
+  printf("Server Step 5: Reading SYN\n");
+  if(read(from_client,private_pipe,HANDSHAKE_BUFFER_SIZE)==-1){
+    perror("Server: Error reading SYN\n");
+    exit(1);
+  }
+  printf("Server received PP: %s\n",private_pipe);
+
+  printf("Server Step 6: Opening PP\n");
+  *to_client = open(private_pipe,O_WRONLY);
+  if(*to_client==-1){
+    perror("Server: Error opening PP\n");
+    exit(1);
+  }
+
+  char syn_ack[] = "SYN_ACK";
+  printf("Server Step 7: Sending SYN_ACK\n");
+  if(write(*to_client,syn_ack,sizeof(syn_ack))==-1){
+    perror("Server: Error sending SYN_ACK\n");
+    exit(1);
+  }
+
+  char final_ack[HANDSHAKE_BUFFER_SIZE];
+  printf("Server Step 9: Reading final ACK\n");
+  if(read(from_client,final_ack,HANDSHAKE_BUFFER_SIZE)==-1){
+    perror("Server: Error reading final ACK\n");
+    exit(1);
+  }
+  printf("Server: Handshake complete. Final ACK: %s\n",final_ack);
+
+
+  return from_client;
+}
+
 int main() {
   int to_client;
   int from_client;
@@ -26,7 +62,7 @@ int main() {
     perror("Server: Client disconnected\n");
     break;
   }
-  printf("Server: Sending random number: %d\n",randnum);
+  //printf("Server: Sending random number: %d\n",randnum);
   sleep(1);
   }
   close(to_client);
--- persistant_server.c
+++ persistant_server.c
@@ -33,4 +33,3 @@ int main() {
   close(from_client);
 }
 }
-
